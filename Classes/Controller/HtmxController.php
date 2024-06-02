06039a45d804d7387fd1614d82d7da5b1e11d620
Bastian Waidelich <b.waidelich@wwwision.de>
2024-06-02 16:13:30 +0200

    Initial version

---
+++ Classes/Controller/HtmxController.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wwwision\HtmxTest\Controller;
+
+use Neos\ContentRepository\Core\DimensionSpace\DimensionSpacePoint;
+use Neos\ContentRepository\Core\NodeType\NodeTypeName;
+use Neos\ContentRepository\Core\Projection\ContentGraph\ContentSubgraphInterface;
+use Neos\ContentRepository\Core\Projection\ContentGraph\Filter\FindDescendantNodesFilter;
+use Neos\ContentRepository\Core\Projection\ContentGraph\Filter\Pagination\Pagination;
+use Neos\ContentRepository\Core\Projection\ContentGraph\VisibilityConstraints;
+use Neos\ContentRepository\Core\SharedModel\ContentRepository\ContentRepositoryId;
+use Neos\ContentRepository\Core\SharedModel\Node\NodeAggregateId;
+use Neos\ContentRepository\Core\SharedModel\Workspace\WorkspaceName;
+use Neos\ContentRepositoryRegistry\ContentRepositoryRegistry;
+use Neos\Flow\Mvc\View\ViewInterface;
+use Neos\Fusion\View\FusionView;
+use Neos\Neos\Controller\Module\AbstractModuleController;
+use Neos\Neos\Domain\Service\NodeTypeNameFactory;
+
+/**
+ * Neos backend module controller for this package
+ */
+final class HtmxController extends AbstractModuleController
+{
+    protected $defaultViewObjectName = FusionView::class;
+
+    /**
+     * how many numbers to load at once in the list view {@see indexAction}
+     */
+    private const NODES_PER_PAGE = 20;
+
+    public function __construct(
+        private readonly ContentRepositoryRegistry $contentRepositoryRegistry,
+    ) {
+    }
+
+    protected function initializeView(ViewInterface $view): void
+    {
+        parent::initializeView($view);
+        // If we're in a HTMX-request...
+        // note: we don't use the "HX-Request" http header to detect this case, so that we can test this case more easily
+        //if ($this->request->hasArgument('htmx')) {
+        if ($this->request->getHttpRequest()->hasHeader('HX-Request')) {
+            // We append an "/htmx" segment to the fusion path, changing it from "<PackageKey>/<ControllerName>/<ActionName>" to "<PackageKey>/<ControllerName>/<ActionName>/htmx"
+            $htmxFusionPath = str_replace(['\\Controller\\', '\\'], ['\\', '/'], $this->request->getControllerObjectName());
+            $htmxFusionPath .= '/' . $this->request->getControllerActionName() . '/htmx';
+            $view->setOption('fusionPath', $htmxFusionPath);
+        }
+    }
+
+    public function indexAction(int $page = 1): void
+    {
+        $subgraph = $this->getDefaultContentSubgraph();
+        $rootNode = $subgraph->findRootNodeByType(NodeTypeName::fromString(NodeTypeNameFactory::NAME_SITES));
+        assert($rootNode !== null);
+        $this->view->assign('page', $page);
+        $this->view->assign('nodes', $subgraph->findDescendantNodes(
+            $rootNode->aggregateId,
+            FindDescendantNodesFilter::create(
+                pagination: Pagination::fromLimitAndOffset(self::NODES_PER_PAGE, self::NODES_PER_PAGE * ($page - 1)),
+            )
+        ));
+    }
+
+    public function showAction(string $id): void
+    {
+        $subgraph = $this->getDefaultContentSubgraph();
+        $this->view->assign('node', $subgraph->findNodeById(NodeAggregateId::fromString($id)));
+    }
+
+    private function getDefaultContentSubgraph(): ContentSubgraphInterface
+    {
+        // This is hard-coded to use the "default" content repository to keep things simple
+        $contentRepository = $this->contentRepositoryRegistry->get(ContentRepositoryId::fromString('default'));
+        // And we are only interested in nodes from the "live" workspace
+        return $contentRepository->getContentGraph(WorkspaceName::forLive())->getSubgraph(
+            // ...and this is hard-coded to the default dimension of the Neos.Demo package!
+            DimensionSpacePoint::fromArray(['language' => 'en_US']),
+            // ...and we load disabled nodes, too, since we're in a backend module
+            VisibilityConstraints::withoutRestrictions()
+        );
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Wwwision.HtmxTest
+
 Simple package to test HTMX integration for Neos backend modules
+
+## Disclaimer
+
+This is just an example that is not meant to be used in production
